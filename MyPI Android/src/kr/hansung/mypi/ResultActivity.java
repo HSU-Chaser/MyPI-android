577e5a1479f56633d711508036920518c5845a33
tera16 <tera16@naver.com>
2014-06-14 18:42:42 +0900

    FirstCommit - resultPage

---
+++ MyPI Android/src/kr/hansung/mypi/ResultActivity.java
@@ -0,0 +1,23 @@
+package kr.hansung.mypi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+
+
+	
+		
+	}
+
+	
+	
+	
+	
+	
+}
